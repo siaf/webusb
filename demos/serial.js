918c7720c86946e18e94808b1deb2d0dc2ead177
Siavash Fathi <siavash.fathi@gmail.com>
2022-08-18 13:53:07 -0400

    Update serial.js

--- demos/serial.js
+++ demos/serial.js
@@ -11,6 +11,7 @@ var serial = {};
 
   serial.requestPort = function() {
     const filters = [
+      { 'vendorId': 0x0483, 'productId': 0x5740 }, // Daisy
       { 'vendorId': 0x2341, 'productId': 0x8036 }, // Arduino Leonardo
       { 'vendorId': 0x2341, 'productId': 0x8037 }, // Arduino Micro
       { 'vendorId': 0x2341, 'productId': 0x804d }, // Arduino/Genuino Zero
